167ac2b6099292f9d4b397bed006a7fc74359a46
caitzpp <caitlyn.zuppinger@outlook.com>
2025-10-26 20:18:25 +0100

    feat(login): tried separating the pages, but still not the exact structure yet

--- pages/login.py
+++ pages/login.py
@@ -1,20 +1,27 @@
 import streamlit as st
 import dotenv
 import os
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 
 dotenv.load_dotenv()
 
+
 def login_page():
-    st.title("Login to Your App")
-    
-    username = st.text_input("Username")
-    password = st.text_input("Password", type='password')
-    
-    if st.button("Login"):
-        if check_login(username, password):  # You would define check_login based on your criteria
-            st.success("Logged in successfully!")
-        else:
-            st.error("Invalid credentials")
+    with open('login_info.yaml') as file:
+        login_info = yaml.load(file, Loader=SafeLoader)
+
+    authenticator = stauth.Authenticate(
+        login_info['credentials'],
+        login_info['cookie']['name'],
+        login_info['cookie']['key'],
+        login_info['cookie']['expiry_days']
+    )
+
+    if not st.user.is_logged_in:
+        # log_in = st.navigation("Login")
+        authenticator.login()
 
 def check_login(username, password):
     # Replace this with your actual login logic (database, API calls, etc.)
