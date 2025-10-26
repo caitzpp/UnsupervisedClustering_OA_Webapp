8f2459971af1a01af05e5eb5c0d7c8cbd7d4ac79
caitzpp <caitlyn.zuppinger@outlook.com>
2025-10-26 21:06:09 +0100

    feat(login)

--- pages/login.py
+++ pages/login.py
@@ -1,11 +1,9 @@
 import streamlit as st
-import dotenv
 import os
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 
-dotenv.load_dotenv()
 
 
 def login_page():
@@ -13,19 +11,14 @@ def login_page():
         login_info = yaml.load(file, Loader=SafeLoader)
 
     authenticator = stauth.Authenticate(
-        login_info['credentials'],
-        login_info['cookie']['name'],
-        login_info['cookie']['key'],
-        login_info['cookie']['expiry_days']
+        credentials=login_info['credentials'],
+        cookie_name=login_info['cookie']['name'],
+        cookie_key=login_info['cookie']['key'],
+        cookie_expiry_days=login_info['cookie']['expiry_days'],
+
     )
 
-    if not st.user.is_logged_in:
-        # log_in = st.navigation("Login")
-        authenticator.login()
+    authenticator.login()
 
-def check_login(username, password):
-    # Replace this with your actual login logic (database, API calls, etc.)
-    return username == os.getenv("USERNAME") and password == os.getenv("PASSWORD")
-
-if __name__ == "__main__":
-    login_page()
+# if __name__ == "__main__":
+#     login_page()
--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,22 @@ import os
 
 from pages import app_page, login
 
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
 if __name__ == "__main__":
-    if not st.user.is_logged_in:
+    if not st.session_state.logged_in:
         login.login_page()
 
-        st.stop()
-    pg = app_page.run_app()
-    pg.run()
+        if st.session_state.get('authentication_status'):
+            st.session_state.logged_in = True
+        elif st.session_state.get('authentication_status') is False:
+            st.error("Username/password is incorrect")
+            st.stop()
+        elif st.session_state.get('authentication_status') is None:
+            st.warning("Please enter your username and password")
+            st.stop()
+    
+    if st.session_state.logged_in:
+        pg = app_page.run_app()
+        pg.run()
