c5b86521dfec94dc10fc67f130de189f90b851e7
caitzpp <caitlyn.zuppinger@outlook.com>
2025-10-26 13:48:16 +0100

    feat(icons): icons set

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,16 @@ from pages import plotly_page, cluster_page, home, about
 
 
 home_page = st.Page(home.show_home, title = "Home", icon=":material/home:")
-graph_page = st.Page(plotly_page.show_graph, title = "Embedding Explorer")
-as_page = st.Page(cluster_page.show_clusterpage, title = "Cluster Gallery")
-about_page = st.Page(about.show_about, title = "About")
+graph_page = st.Page(plotly_page.show_graph, title = "Embedding Explorer", icon=":material/biotech:")
+as_page = st.Page(cluster_page.show_clusterpage, title = "Cluster Gallery", icon=":material/imagesmode:")
+about_page = st.Page(about.show_about, title = "About", icon=":material/info:")
 
 
 pg = st.navigation([home_page, graph_page, as_page, about_page])
 st.set_page_config(layout="wide") #page_title="Cluster Dashboard", page_icon=":material/dashboard:", 
  
+ #icon logout
+
 
 if __name__ == "__main__":
     pg.run()
