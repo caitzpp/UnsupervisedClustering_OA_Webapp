ade7cef1b594354c5839fceec58e1a80afbfea22
caitzpp <caitlyn.zuppinger@outlook.com>
2025-10-26 21:08:39 +0100

    feat(login): now it appears to be working

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,22 +12,17 @@ import os
 
 from pages import app_page, login
 
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
 
 if __name__ == "__main__":
-    if not st.session_state.logged_in:
-        login.login_page()
+    login.login_page()
 
-        if st.session_state.get('authentication_status'):
-            st.session_state.logged_in = True
-        elif st.session_state.get('authentication_status') is False:
-            st.error("Username/password is incorrect")
-            st.stop()
-        elif st.session_state.get('authentication_status') is None:
-            st.warning("Please enter your username and password")
-            st.stop()
+    if st.session_state.get('authentication_status') is False:
+        st.error("Username/password is incorrect")
+        st.stop()
+    elif st.session_state.get('authentication_status') is None:
+        st.warning("Please enter your username and password")
+        st.stop()
     
-    if st.session_state.logged_in:
-        pg = app_page.run_app()
-        pg.run()
+
+    pg = app_page.run_app()
+    pg.run()
