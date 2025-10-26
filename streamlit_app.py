9d015d914a0f5cbb19cd64b7a3e58a2d8aeec4b8
caitzpp <caitlyn.zuppinger@outlook.com>
2025-10-26 13:44:54 +0100

    cleanup

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,6 @@ import config
 from pages import plotly_page, cluster_page, home, about
 
 
-# selected = option_menu(
-#     menu_title = "Main Menu",
-#     options = ["Home","Embedding Explorer", "Cluster Gallery", "About"],
-#     icons = [":material/Home:","microscope","art", "info-circle"],
-#     menu_icon = "cast",
-#     default_index = 0,
-#     #orientation = "horizontal",
-# )
-    
 home_page = st.Page(home.show_home, title = "Home", icon=":material/home:")
 graph_page = st.Page(plotly_page.show_graph, title = "Embedding Explorer")
 as_page = st.Page(cluster_page.show_clusterpage, title = "Cluster Gallery")
@@ -26,35 +17,6 @@ about_page = st.Page(about.show_about, title = "About")
 
 pg = st.navigation([home_page, graph_page, as_page, about_page])
 st.set_page_config(layout="wide") #page_title="Cluster Dashboard", page_icon=":material/dashboard:", 
-
-# if selected == "Home":
-#     st.header('xxx')
-
-#     with open("content/home.md", "r", encoding="utf-8") as f:
-#         markdown_text = f.read()
-#     st.markdown(markdown_text, unsafe_allow_html=True)
-#     # Load markdown file
-#     # with open("README.md", "r", encoding="utf-8") as f:
-#     #     markdown_text = f.read()
-
-#     # # Display it in the app
-#     # st.markdown(markdown_text, unsafe_allow_html=True)
-   
-        
-    
-# if selected == "Embedding Explorer":
-#     st.header('Embedding Explorer')
-#     iframe(config.PLOTLY_URL, width=1800, height=1000)
-  
-
-    
-# # if selected == "Cluster Gallery":
-
-# if selected == "About":
-#     st.header('About')
-#     with open("content/about.md", "r", encoding="utf-8") as f:
-#         markdown_text = f.read()
-#     st.markdown(markdown_text, unsafe_allow_html=True)
  
 
 if __name__ == "__main__":
