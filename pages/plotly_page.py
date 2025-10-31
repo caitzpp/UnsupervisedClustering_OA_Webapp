f8cfe2f5570a5ef88a8eb15c1fa83ee4b40b8ddc
caitzpp <caitlyn.zuppinger@outlook.com>
2025-10-31 11:17:29 +0100

    feat(google_forms): added logic to embed google forms directly into app if necessary

--- pages/plotly_page.py
+++ pages/plotly_page.py
@@ -4,4 +4,13 @@ from streamlit.components.v1 import iframe
 
 def show_graph():
     st.header('Embedding Explorer')
-    iframe(config.PLOTLY_URL, width=700, height=600)
+    iframe(config.PLOTLY_URL, width=1400, height=1200)
+
+    # form_url = "https://docs.google.com/forms/d/e/1FAIpQLSe7q4a-ze55KQfCmE1tGtyN42_UkGl_LYXAcgKXGnPiSPbq6Q/viewform?embedded=true"
+    # st.title("Feedback Form")
+    # st.markdown(
+    #         f"""
+    #     <iframe src="{form_url}" width="640" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    #     """,
+    #     unsafe_allow_html=True,
+    # )
