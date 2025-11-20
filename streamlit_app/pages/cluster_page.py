bfdbc98aec37bfe45382935f9ee7b07708a4b7eb
caitzpp <caitlyn.zuppinger@outlook.com>
2025-11-20 14:34:20 +0100

    feat(feedbackform): tried to improve setting of feedback form. For plotly page it looks fine, however cluster page doesn't work yet

--- streamlit_app/pages/cluster_page.py
+++ streamlit_app/pages/cluster_page.py
@@ -72,7 +72,8 @@ def show_clusterpage():
             
     st.markdown(f"### Showing {len(df_cluster[:max_n])} images for Cluster {selected_cluster}")
 
-    cols = st.columns(5)
+    n_cols = 3
+    cols = st.columns(n_cols + 2)
 
     for i, (_, row) in enumerate(df_cluster.head(max_n).iterrows()):
         img_path = row['id'] + '.png'
@@ -82,32 +83,33 @@ def show_clusterpage():
             encoded = base64.b64encode(blob_data).decode("utf-8")
             img_path = f"data:image/png;base64,{encoded}"
             print(f"ImgPath exists {img_path}")
-            with cols[i % 5]:
+            with cols[i % n_cols]:
                 st.image(str(img_path), use_container_width=True)
         # else:
         #     with cols[i % 5]:
         #         st.caption(f"Missing: {img_path}")
 
-    html("""
-    <script async src="https://tally.so/widgets/embed.js"></script>
-     <button 
-        data-tally-open="XxlEkL" 
-        data-tally-width="1000" 
-        data-tally-emoji-text="👋" 
-        data-tally-emoji-animation="wave"
-        style="
-            background-color: #4CAF50;
-            color: white;
-            padding: 10px 20px;
-            font-size: 16px;
-            border: none;
-            border-radius: 8px;
-            cursor: pointer;
-        "
-        >
-        Open Feedback Form
-        </button>
-    """, height = 1000)
+    with cols[n_cols-2:n_cols+2]:
+        html("""
+        <script async src="https://tally.so/widgets/embed.js"></script>
+        <button 
+            data-tally-open="XxlEkL" 
+            data-tally-width="1000" 
+            data-tally-emoji-text="👋" 
+            data-tally-emoji-animation="wave"
+            style="
+                background-color: #4CAF50;
+                color: white;
+                padding: 10px 20px;
+                font-size: 16px;
+                border: none;
+                border-radius: 8px;
+                cursor: pointer;
+            "
+            >
+            Open Feedback Form
+            </button>
+        """, height = 1000)
 
 
 if __name__ == "__main__":
--- streamlit_app/pages/plotly_page.py
+++ streamlit_app/pages/plotly_page.py
@@ -2,19 +2,7 @@ import streamlit as st
 import config
 from streamlit.components.v1 import iframe, html
 
-def show_modal():
-    if "force_modal" not in st.session_state:
-        st.session_state.force_modal = True
 
-    if st.button("Open Feedback Form"):
-        st.session_state.force_modal = True
-
-    if st.session_state.force_modal:
-            with st.modal("Feedback Form"):
-                iframe(
-                    "https://tally.so/r/mOpM5M",
-                    height=650
-                )
 
 def show_graph():
     st.header('Embedding Explorer')
@@ -24,57 +12,39 @@ def show_graph():
 
     st.markdown(markdown_text, unsafe_allow_html=True)  
 
-    show_modal() 
-    
-    # html(
-    # #     """<script async src="https://tally.so/widgets/embed.js"></script>
+    col1, col2 = st.columns([3, 2])
+    with col1:
+        iframe(config.PLOTLY_URL, width=1400, height=1200)
 
-    # # <script>
-    # # function openTallyWhenReady() {
-    # #     // If Tally exists, open the popup
-    # #     if (window.Tally) {
-    # #         Tally.openPopup('mOpM5M', {
-    # #             width: 1000,
-    # #             hideTitle: false,
-    # #             overlay: true,
-    # #             autoClose: false
-    # #         });
-    # #     } else {
-    # #         // Try again after 200ms
-    # #         setTimeout(openTallyWhenReady, 200);
-    # #     }
-    # # }
+    with col2:
+        html(
 
-    # # // Ensure we run only after the iframe DOM is ready
-    # # setTimeout(openTallyWhenReady, 300);
-    # # </script>
-    # #     """
-    #     """
-    #     <script async src="https://tally.so/widgets/embed.js"></script>
+            """
+            <script async src="https://tally.so/widgets/embed.js"></script>
 
-    #     <!-- Add your button with the data attributes -->
-    #     <button 
-    #     data-tally-open="mOpM5M" 
-    #     data-tally-width="1000" 
-    #     data-tally-emoji-text="👋" 
-    #     data-tally-emoji-animation="wave"
-    #     style="
-    #         background-color: #4CAF50;
-    #         color: white;
-    #         padding: 10px 20px;
-    #         font-size: 16px;
-    #         border: none;
-    #         border-radius: 8px;
-    #         cursor: pointer;
-    #     "
-    #     >
-    #     Open Feedback Form
-    #     </button>
-    #     """
-    #      , height=10, 
-    #    # scrolling=True
-    #     )
+            <!-- Add your button with the data attributes -->
+            <button 
+            data-tally-open="mOpM5M" 
+            data-tally-width="1000" 
+            data-tally-emoji-text="👋" 
+            data-tally-emoji-animation="wave"
+            style="
+                background-color: #4CAF50;
+                color: white;
+                padding: 10px 20px;
+                font-size: 16px;
+                border: none;
+                border-radius: 8px;
+                cursor: pointer;
+            "
+            >
+            Open Feedback Form
+            </button>
+            """
+            , height=1200, 
+        # scrolling=True
+            )
 
-    iframe(config.PLOTLY_URL, width=1400, height=1200)
+
 
  
